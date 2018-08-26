9f7b086d4f3cc7c43a9b36a9ca82a4f4b4fb6cf4
vannoel <vannoel0628@gmail.com>
2018-08-26 16:24:53 +0800

    [update]  1. upgrade blacktbox-table for adding feature searching table in list mode. (example with simple searching Controller component)

--- src/routes/blacktbox-table/exampleList.js
+++ src/routes/blacktbox-table/exampleList.js
@@ -31,29 +31,47 @@ const ROURCECODE_RENDER_PRE =
 />`;
 const ROURCECODE_HEADER_PRE = 
 `tableHeaderArr = [
-  {name: 'Name',        index: 'name'},
-  {name: 'Description', index: 'desc'},
-  {name: 'OS Type',     index: 'osType'},
-  {name: 'IP Address',  index: 'ipAddr'},
-  {name: 'MAC Address', index: 'macAddr'},
-  {name: 'Statue',      index: 'status'}
+  {name: 'Name',              index: 'name',     },
+  {name: 'Type',              index: 'devType',  },
+  {name: 'OS',                index: 'osType',   },
+  {name: 'IP',                index: 'ipAddr',   },
+  {name: 'MAC',               index: 'macAddr',  },
+  {name: 'Traffic(tx / rx)',  index: 'traffic',  },
+  {name: 'Statue',            index: 'statusDesc'}
 ];`;
 const ROURCECODE_BODY_PRE = 
 `tableBodyArr = [
-  {
-    name: 'Device 1', 
-    desc: 'My phone', 
-    osType: 'IOS', 
-    ipAddr: '192.168.0.50', 
-    macAddr: '11:22:33:44:55:66', 
-    status: 'Disconnected'
-  }
+  {name: 'Device 1',  devType: 'phone',   osType: 'ios',      ipAddr: '192.168.0.50',   macAddr: 'B4:A2:07:44:55:2A', traffic: '0 MB / 0 KB',     status: 0, statusDesc: 'Disconnected'},
+  {name: 'Device 2',  devType: 'switch',  osType: 'linux',    ipAddr: '192.168.0.37',   macAddr: '7E:EF:02:44:AE:25', traffic: '2.7 MB / 263 KB', status: 1, statusDesc: 'Connected'},
+  {name: 'Device 3',  devType: 'ap',      osType: 'linux',    ipAddr: '192.168.0.121',  macAddr: 'EF:2B:15:44:32:B4', traffic: '0 MB / 0 KB',     status: 2, statusDesc: 'Locked'},
+  {name: 'Device 4',  devType: 'phone',   osType: 'android',  ipAddr: '192.168.0.9',    macAddr: 'F5:22:33:44:55:35', traffic: '0 MB / 0 KB',     status: 0, statusDesc: 'Disconnected'},
+  {name: 'Device 5',  devType: 'ap',      osType: 'linux',    ipAddr: '192.168.0.27',   macAddr: '7E:EF:B2:44:28:3B', traffic: '1.3 MB / 725 KB', status: 1, statusDesc: 'Connected'},
+  {name: 'Device 6',  devType: 'pc',      osType: 'windows',  ipAddr: '192.168.0.11',   macAddr: '11:22:24:44:5E:90', traffic: '0 MB / 0 KB',     status: 2, statusDesc: 'Locked'},
+  {name: 'Device 7',  devType: 'phone',   osType: 'ios',      ipAddr: '192.168.0.6',    macAddr: '11:FF:33:44:55:A3', traffic: '1.2 MB / 45 KB',  status: 1, statusDesc: 'Connected'},
+  {name: 'Device 8',  devType: 'pc',      osType: 'linux',    ipAddr: '192.168.0.3',    macAddr: 'B2:FE:B8:44:55:6D', traffic: '4.8 MB / 3.7 MB', status: 1, statusDesc: 'Connected'},
+  {name: 'Device 9',  devType: 'pc',      osType: 'windows',  ipAddr: '192.168.0.18',   macAddr: 'E4:AA:74:44:38:E1', traffic: '0 MB / 0 KB',     status: 2, statusDesc: 'Locked'},
+  {name: 'Device 10', devType: 'switch',  osType: 'linux',    ipAddr: '192.168.0.245',  macAddr: 'E2:BA:33:44:48:AB', traffic: '0 MB / 0 KB',     status: 0, statusDesc: 'Disconnected'},
+  {name: 'Device 11', devType: 'ap',      osType: 'linux',    ipAddr: '192.168.0.210',  macAddr: '2A:FE:7A:27:38:27', traffic: '0 MB / 0 KB',     status: 0, statusDesc: 'Disconnected'},
+  {name: 'Device 12', devType: 'phone',   osType: 'android',  ipAddr: '192.168.0.163',  macAddr: 'FE:22:9B:44:26:08', traffic: '0 MB / 0 KB',     status: 2, statusDesc: 'Locked'},
+  {name: 'Device 13', devType: 'phone',   osType: 'android',  ipAddr: '192.168.0.84',   macAddr: 'B4:22:27:44:55:B2', traffic: '1.5 MB / 235 KB', status: 1, statusDesc: 'Connected'}
 ];`;
 const ROURCECODE_MSG_PRE =
 `noDataStr = 'No data.';`;
 const ROURCECODE_MODE_PRE = 
 `modeObj = {
-  mode : 'list'
+  mode : 'list',
+  listFeatureSearch: {
+    enable: true,
+    searchInputArr: [],
+    searchMatchRateTheshold: 0,
+    searchSpecAttributeEnable: true,
+    searchSpecAttributeArr: ['name', 'devType', 'osType', 'ipAddr', 'macAddr', 'status']
+  },
+  listFeaturePage: {
+    enable: true,
+    dataPerPage: 10,
+    pageIndex: 1
+  }
 };`;
 const ROURCECODE_STYLE_PRE = 
 `styleObj = {
@@ -118,6 +136,13 @@ const tableBodyArr = [
 const noDataStr = 'No data.';
 var modeObj = {
   mode : 'list',
+  listFeatureSearch: {
+    enable: true,
+    searchInputArr: [],
+    searchMatchRateTheshold: 0,
+    searchSpecAttributeEnable: true,
+    searchSpecAttributeArr: ['name', 'devType', 'osType', 'ipAddr', 'macAddr', 'status']
+  },
   listFeaturePage: {
     enable: true,
     dataPerPage: 10,
@@ -154,6 +179,10 @@ class Example extends Component {
   constructor (props) {
     super(props);
     this.listRef =[];
+    this.env = {
+      searchInputArr: [],
+      searchMatchRateTheshold: 0
+    };
   };
   render () {
     let content = [];
@@ -164,6 +193,7 @@ class Example extends Component {
         <AL.Section>
           <AL.SectionTitle>{exampleTitle}</AL.SectionTitle>
           <AL.Content>
+            {this.searchConfigRender()}
             <BTBTable 
               tableHeadArr={tableHeaderArr}
               tableBobyArr={tableBodyArr}
@@ -172,7 +202,7 @@ class Example extends Component {
               styleObj={styleObj}
               refFn={(ref)=>{this.listRef=ref}}
             />
-            {this.temp_pagingConfigRender()}
+            {this.pageConfigRender()}
           </AL.Content>
           <AL.Item>
             <button onClick={()=>{this._consoleLogRef()}}>{exampleButtonText}</button>
@@ -214,8 +244,58 @@ class Example extends Component {
     let listRef = ReactDOM.findDOMNode(this.listRef);
     console.log('listRef', listRef)
   };
-  
-  temp_pagingConfigRender () {
+
+  searchConfigRender () {
+    let content = [];
+    let searchInput = this.env.searchInputArr.join(' ');
+    let searchMatchRate = this.env.searchMatchRateTheshold;
+    content.push(
+      <div
+        style={{'margin-bottom': '10px', 'text-align': 'left'}}
+      >
+      {
+        (()=>{
+          // data information
+          let content_input = [];
+          content_input.push(
+            <div>
+              {`Search Input: `}
+              <input type='text' style={{'font-size': '14px'}} value={searchInput} onChange={(event)=>{this._searchInputHandler(event)}} placeholder='Search Input' />
+            </div>
+          );
+          return content_input;
+        })()
+      }
+      {
+        (()=>{
+          // data information
+          let content_matchedRate = [];
+          content_matchedRate.push(
+            <div>
+              {`Search Matched Rate Theshold: `}
+              <input type='text' style={{'font-size': '14px'}} value={searchMatchRate} onChange={(event)=>{this._searchMatchRateHandler(event)}} placeholder='Search Matched Rate Theshold' />
+            </div>
+          );
+          return content_matchedRate;
+        })()
+      }
+      {
+        (()=>{
+          // data information
+          let content_searchBtn = [];
+          content_searchBtn.push(
+            <div>
+              <button onClick={()=>{this._searchHandler()}}>Search</button>
+            </div>
+          );
+          return content_searchBtn;
+        })()
+      }
+      </div>
+    );
+    return content;
+  };
+  pageConfigRender () {
     let content = [];
     let dataTotal = tableBodyArr.length;
     let pageIndex = modeObj.listFeaturePage.pageIndex;
@@ -308,6 +388,29 @@ class Example extends Component {
     return content;
   };
 
+  _searchInputHandler (event) {
+    this.env.searchInputArr = event.target.value.split(' ');
+    this.forceUpdate();
+  };
+  _searchMatchRateHandler (event) {
+    this.env.searchMatchRateTheshold = event.target.value;
+    this.forceUpdate();
+  };
+  _searchHandler () {
+    let matchedRate = parseFloat(this.env.searchMatchRateTheshold);
+    if ((0 <= matchedRate) 
+      && (1 >= matchedRate)
+    )
+    {
+      modeObj.listFeatureSearch.searchInputArr = this.env.searchInputArr.slice(0);
+      modeObj.listFeatureSearch.searchMatchRateTheshold = matchedRate;
+      this.forceUpdate();
+    }
+    else
+    {
+      alert('Remind: Search Match Rate Theshold must be float between 0-1');
+    };
+  };
   _perPageSelectHandler (event) {
     modeObj.listFeaturePage.dataPerPage = event.target.value;
     modeObj.listFeaturePage.pageIndex = 1;
